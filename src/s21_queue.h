5d07cb10f0469e7252b6181bd9f567849f2cc127
joserans <joserans@student.21-school.ru>
2023-10-05 10:25:36 +0300

    queue start

--- src/s21_queue.h
+++ src/s21_queue.h
@@ -1,6 +1,30 @@
 #ifndef CPP2_SRC_S21_QUEUE_H_
 #define CPP2_SRC_S21_QUEUE_H_
 
+#include "stdexcept"
 
+namespace s21 {
+
+    template<typename T>
+    class Queue {
+
+    public:
+        // attributes
+        using value_type = T;
+        using reference = value_type &;
+        using const_reference = const value_type &;
+        using size_type = std::size_t;
+
+    private:
+
+        struct Node {
+            value_type value;
+            Node *prev;
+            Node *next;
+
+            explicit Node(const_reference value) : value(value), prev(nullptr), next(nullptr) {}
+        };
+    };
+}
 
 #endif  // CPP2_SRC_S21_QUEUE_H_
