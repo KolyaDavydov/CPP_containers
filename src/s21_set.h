9fa5a18e743d36ba17560a08e16bbef37262b119
nohoteth <nohoteth@student.21-school.ru>
2023-10-12 21:03:16 +0300

    change iterator type

--- src/s21_set.h
+++ src/s21_set.h
@@ -1,6 +1,6 @@
 #ifndef CPP2_SRC_S21_SET_H_
 #define CPP2_SRC_S21_SET_H_
-#include "tree_set.h"
+#include "tree.h"
 
 namespace s21 {
 
@@ -13,18 +13,20 @@ class set {
     using value_type = Key;
     using reference = value_type &;
     using const_reference = const value_type &;
-    using tree_type = Tree<value_type>;
-    using iterator = typename tree_type::Iterator;
-    using const_iterator = const typename tree_type::Iterator;
+    using tree_type = Tree<key_type, value_type>;
+    using iterator = Iterator<Key>;
+    // using iterator = SetIterator<Key>;
+    // using const_iterator = SetConstIterator<Key>;
     using size_type = std::size_t;
 
+    // tree_type *tree_;
     set() : tree_(new tree_type{}) {}
 
     set(std::initializer_list<value_type> const &items) {
       for (value_type i : items) insert(i);
     }
 
-    set(const Set &v) {}
+    set(const set &v) {}
 
     set(const set &other) : tree_(new tree_type(*other.tree_)) {}
 
@@ -38,18 +40,42 @@ class set {
       tree_ = nullptr;
     }
 
-        std::pair<iterator, bool> insert(const value_type &value) {
+    std::pair<iterator, bool> insert(const value_type &value) {
       return tree_->Insert(value);
     }
     // iterators
 
-    iterator begin() noexcept { return tree_->begin(); }
+    iterator begin() noexcept {
+      Node<key_type, value_type> *node = this->tree_.GetRoot();
+      while (node->left != nullptr) {
+        node = node->left;
+      }
+      return iterator(node, tree_.GetRoot());
+    }
 
-    const_iterator begin() const noexcept { return tree_->begin(); }
+    const_iterator begin() const noexcept {
+      Node<key_type, value_type> *node = this->tree_.GetRoot();
+      while (node->left != nullptr) {
+        node = node->left;
+      }
+      return iterator(node, tree_.GetRoot());
+    }
 
-    iterator end() noexcept { return tree_->end(); }
+    iterator end() noexcept {
+      Node<key_type, value_type> *node = tree_.GetRoot();
+      while (node->right != nullptr) {
+        node = node->right;
+      }
+      return iterator(node, tree_.GetRoot());
+    }
 
-    const_iterator end() const noexcept { return tree_->end(); }
+    const_iterator end() const noexcept {
+      Node<key_type, value_type> *node = tree_.GetRoot();
+      while (node->right != nullptr) {
+        node = node->right;
+      }
+      return iterator(node, tree_.GetRoot());
+    }
 
     iterator find(const key_type &key) noexcept { return tree_->Search(key); }
 
@@ -70,6 +96,23 @@ class set {
       *tree_ = std::move(*other.tree_);
       return *this;
     }
+    // void erase(iterator pos);
+
+    // void swap(set & other);
+
+    // void merge(set & other);
+
+    bool empty() {
+      if (this->tree_.GetSize() == 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    size_type size() { return this->tree_.GetSize(); }
+
+    size_type max_size() { return this->tree_.MaxSize(); }
   }
 
  private:
